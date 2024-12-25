29afa424023460f9cda6e443f3779cb1197623db
Tim Platzer <tim.platzer@gmx.at>
2024-12-26 00:57:24 +0100

    added: - attributes as well as getter and setter and a Constructor - reveal and toggleFlag function

--- src/main/java/htl/steyr/javafx_minesweeper_tplatzer/Cell.java
+++ src/main/java/htl/steyr/javafx_minesweeper_tplatzer/Cell.java
@@ -1,8 +1,134 @@
 package htl.steyr.javafx_minesweeper_tplatzer;
 
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
 
 public class Cell
 {
+    private boolean isBomb;
+    private boolean isFlagged;
+    private boolean isRevealed;
+    private int adjacentBombs;
+    private Button button;
+    private GameController controller;
+    private int row;
+    private int col;
 
+    public Cell (boolean isBomb, GameController controller, int row, int col)
+    {
+        setBomb(isBomb);
+        setController(controller);
+        setRow(row);
+        setCol(col);
+
+        setFlagged(false);
+        setRevealed(false);
+        setAdjacentBombs(0);
+
+        setButton(new Button());
+        getButton().setPrefSize(30, 30);
+        getButton().getStyleClass().add("cell-button");
+
+        getButton().setOnMouseClicked(event ->
+        {
+            switch (event.getButton())
+            {
+                case MouseButton.PRIMARY -> reveal();
+                case MouseButton.SECONDARY -> toggleFlag();
+            }
+        });
+    }
+
+    private void reveal()
+    {
+
+    }
+
+    private void toggleFlag()
+    {
+
+    }
+
+
+
+
+    public boolean isBomb()
+    {
+        return isBomb;
+    }
+
+    public void setBomb(boolean bomb)
+    {
+        isBomb = bomb;
+    }
+
+    public boolean isFlagged()
+    {
+        return isFlagged;
+    }
+
+    public void setFlagged(boolean flagged)
+    {
+        isFlagged = flagged;
+    }
+
+    public boolean isRevealed()
+    {
+        return isRevealed;
+    }
+
+    public void setRevealed(boolean revealed)
+    {
+        isRevealed = revealed;
+    }
+
+    public int getAdjacentBombs()
+    {
+        return adjacentBombs;
+    }
+
+    public void setAdjacentBombs(int adjacentBombs)
+    {
+        this.adjacentBombs = adjacentBombs;
+    }
+
+    public Button getButton()
+    {
+        return button;
+    }
+
+    public void setButton(Button button)
+    {
+        this.button = button;
+    }
+
+    public GameController getController()
+    {
+        return controller;
+    }
+
+    public void setController(GameController controller)
+    {
+        this.controller = controller;
+    }
+
+    public int getRow()
+    {
+        return row;
+    }
+
+    public void setRow(int row)
+    {
+        this.row = row;
+    }
+
+    public int getCol()
+    {
+        return col;
+    }
+
+    public void setCol(int col)
+    {
+        this.col = col;
+    }
 }
