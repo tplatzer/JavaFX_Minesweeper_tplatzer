678c457c0765affe9f81c29d3da37499a7e7bc1c
Tim Platzer <tim.platzer@gmx.at>
2024-12-29 20:09:48 +0100

    removed - unused import statements

--- src/main/java/htl/steyr/javafx_minesweeper_tplatzer/MenuController.java
+++ src/main/java/htl/steyr/javafx_minesweeper_tplatzer/MenuController.java
@@ -1,12 +1,9 @@
 package htl.steyr.javafx_minesweeper_tplatzer;
 
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
