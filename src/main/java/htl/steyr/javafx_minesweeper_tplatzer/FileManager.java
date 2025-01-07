f3bdf5555940938c0270cc67a87841f9e80e613b
Tim Platzer <tim.platzer@gmx.at>
2025-01-07 08:05:24 +0100

    implemented - fallback for saving user_data in linux and macOS

--- src/main/java/htl/steyr/javafx_minesweeper_tplatzer/FileManager.java
+++ src/main/java/htl/steyr/javafx_minesweeper_tplatzer/FileManager.java
@@ -14,16 +14,30 @@ public class FileManager
 
     public static File getAppDataFolder()
     {
-        String appDataPath = System.getenv("LOCALAPPDATA");
-        if (appDataPath == null)
+        String osName = System.getProperty("os.name").toLowerCase();
+        String appDataPath;
+
+        if (osName.contains("win"))
         {
-            throw new IllegalStateException("LOCALAPPDATA environment variable is not set");
+            appDataPath = System.getenv("LOCALAPPDATA");
+            if (appDataPath == null)
+            {
+                throw new IllegalStateException("LOCALAPPDATA environment variable is not set");
+            }
+        } else if (osName.contains("mac"))
+        {
+            appDataPath = System.getProperty("user.home") + "/Library/Application Support";
+        } else
+        {
+            appDataPath = System.getProperty("user.home") + "/.config";
         }
+
         File appFolder = new File(appDataPath, APP_NAME);
         if (!appFolder.exists())
         {
             appFolder.mkdirs();
         }
+
         return appFolder;
     }
 }
